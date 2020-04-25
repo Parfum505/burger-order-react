f069b28c1b012814ca095a6aed5be96412a024f2
Pawel Kryvytskyi <pawel.kryvytskyi@traveltech.pl>
2020-04-25 18:30:15 +0200

    Updates

---
+++ precache-manifest.50db146842eaa5949aad7e8dc9622f76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262db01f9162a26ebe0cae3a946ff95c",
+    "url": "/burger-order-react/index.html"
+  },
+  {
+    "revision": "13ff7fb7aba0bfec1972",
+    "url": "/burger-order-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5ebfadea73ff6949061f",
+    "url": "/burger-order-react/static/js/2.8304f5c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-order-react/static/js/2.8304f5c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ff7fb7aba0bfec1972",
+    "url": "/burger-order-react/static/js/main.5c1fb515.chunk.js"
+  },
+  {
+    "revision": "5b9e1e0f9a082f6efaed",
+    "url": "/burger-order-react/static/js/runtime-main.992ed215.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burger-order-react/static/media/logo.5d5d9eef.svg"
+  }
+]);

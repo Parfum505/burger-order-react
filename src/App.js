ef4274be5d112bc7ef64181d2d97c7f515010c17
Pavel Kryvytskyi <pavel.kryvytskyi@gmail.com>
2020-05-27 15:47:25 +0200

    add redirect to first page if user not logged in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
@@ -6,40 +8,37 @@ import Authorisation from "./containers/Authorisation/Authorisation";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
 import Logout from "./containers/Authorisation/Logout/Logout";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import burgerBuilderReducer from "./store/reducers/burgerBuilder";
-import orderReducer from "./store/reducers/order";
-import authReducer from "./store/reducers/authorisation";
-import thunk from "redux-thunk";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const rootReducer = combineReducers({
-  burgerBuilder: burgerBuilderReducer,
-  order: orderReducer,
-  auth: authReducer,
-});
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
-
-function App() {
+function App(props) {
+  let routs = (
+    <Switch>
+      <Route path="/authorisation" component={Authorisation} />
+      <Route path="/" component={BurgerBuilder} />
+    </Switch>
+  );
+  if (props.isAuth) {
+    routs = (
+      <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
+        <Route path="/authorisation" component={Authorisation} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/" component={BurgerBuilder} />
+      </Switch>
+    );
+  }
   return (
-    <Provider store={store}>
-      <BrowserRouter basename="/burger-order-react">
-        <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/authorisation" component={Authorisation} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
-        </Layout>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter basename="/burger-order-react">
+      <Layout>{routs}</Layout>
+    </BrowserRouter>
   );
 }
-
-export default App;
+App.propTypes = {
+  isAuth: PropTypes.bool.isRequired,
+};
+const mapStateToProps = (state) => {
+  return {
+    isAuth: state.auth.token !== null,
+  };
+};
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
+import orderReducer from "./store/reducers/order";
+import authReducer from "./store/reducers/authorisation";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer,
+  auth: authReducer,
+});
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
